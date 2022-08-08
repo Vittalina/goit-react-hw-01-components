ed9d33ca6ab5b4d57ac5fc96853d57525945e094
Vittalina <vitalina.alleksandrovna@gmail.com>
2022-08-08 15:04:59 +0300

    commit

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,33 +1,34 @@
 import PropTypes from 'prop-types';
-import { Item, Status, Avatar, FriendName } from 'components/FriendList/FriendList.styled';
-import {checkOnline} from 'components/FriendList/checkOnline'
+import {
+  Item,
+  Status,
+  Avatar,
+  FriendName,
+} from 'components/FriendList/FriendList.styled';
+import { checkOnline } from 'components/FriendList/checkOnline';
 import { Box } from 'components/Box';
 
-export const FriendList = ({friends}) => {
-    return <Box
-        width='300px'
-        mt='30px'
-        ml='auto'
-        mr='auto'
-        borderRadius='8px'
-        as='ul'
-    >
-        {friends.map(({ avatar, name, isOnline, id }) => (
-            <Item key={id}>
-                <Status>{`${checkOnline(isOnline)}`}</Status>
-            <Avatar src={avatar} alt="User avatar" width="48" />
-                <FriendName>{name}</FriendName>
-        </Item>))}
-</Box>
-}
+export const FriendList = ({ friends }) => {
+  return (
+    <Box width="300px" mt="30px" ml="auto" mr="auto" borderRadius="8px" as="ul">
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <Item key={id}>
+          <Status>{`${checkOnline(isOnline)}`}</Status>
+          <Avatar src={avatar} alt="User avatar" width="48" />
+          <FriendName>{name}</FriendName>
+        </Item>
+      ))}
+    </Box>
+  );
+};
 
 FriendList.propTypes = {
-    friends: PropTypes.arrayOf(
-        PropTypes.exact({
-            avatar: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            isOnline: PropTypes.bool.isRequired,
-            id: PropTypes.number.isRequired
-        })
-    )
-}
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,46 +1,51 @@
 import PropTypes from 'prop-types';
-import { TableHead, TableHeadText, TableData} from 'components/TransactionHistory/TransactionHistory.styled';
+import {
+  TableHead,
+  TableHeadText,
+  TableData,
+} from 'components/TransactionHistory/TransactionHistory.styled';
 import { Box } from 'components/Box';
 
-export const TransactionHistory = ({items}) => {
-    return (
-        <Box
-            width='300px'
-            mt='30px'
-            ml='auto'
-            mr='auto'
-            bg='#f5f4fa'
-            textAlign='center'
-            borderRadius='8px'
-            as='table'>
-                <TableHead>
-    <tr>
-        <TableHeadText>Type</TableHeadText>
-        <TableHeadText>Amount</TableHeadText>
-        <TableHeadText>Currency</TableHeadText>
-    </tr>
-    </TableHead>
+export const TransactionHistory = ({ items }) => {
+  return (
+    <Box
+      width="300px"
+      mt="30px"
+      ml="auto"
+      mr="auto"
+      bg="#f5f4fa"
+      textAlign="center"
+      borderRadius="8px"
+      as="table"
+    >
+      <TableHead>
+        <tr>
+          <TableHeadText>Type</TableHeadText>
+          <TableHeadText>Amount</TableHeadText>
+          <TableHeadText>Currency</TableHeadText>
+        </tr>
+      </TableHead>
 
-    <tbody>
+      <tbody>
         {items.map(({ id, type, amount, currency }) => (
-    <tr key={id}>
-        <TableData>{type}</TableData>
-        <TableData>{amount}</TableData>
-        <TableData>{currency}</TableData>
-    </tr>
+          <tr key={id}>
+            <TableData>{type}</TableData>
+            <TableData>{amount}</TableData>
+            <TableData>{currency}</TableData>
+          </tr>
         ))}
-            </tbody>
+      </tbody>
     </Box>
-    )
-}
+  );
+};
 
 TransactionHistory.propTypes = {
-    friends: PropTypes.arrayOf(
-        PropTypes.exact({
-            id: PropTypes.string.isRequired,
-            type: PropTypes.string.isRequired,
-            amount: PropTypes.string.isRequired,
-            currency: PropTypes.string.isRequired
-        })
-    )
-}
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
