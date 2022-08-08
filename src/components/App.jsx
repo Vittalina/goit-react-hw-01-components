f447144e245e4e334b9feebcd68efbe319fdb214
Vittalina <vitalina.alleksandrovna@gmail.com>
2022-08-08 15:01:48 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,22 +7,21 @@ import friends from 'data/friends';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 import transactions from 'data/transactions';
 
-import {Box} from 'components/Box'
+import { Box } from 'components/Box';
 
 export const App = () => {
   return (
-    <Box
-      p={50} px>
+    <Box p={50} px>
       <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />;
-      </Box>
-    );
+    </Box>
+  );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,47 +1,48 @@
 import PropTypes from 'prop-types';
-import { Title, Item, Label, Percentage } from 'components/Statistics/Statistics.styled';
+import {
+  Title,
+  Item,
+  Label,
+  Percentage,
+} from 'components/Statistics/Statistics.styled';
 import { Box } from 'components/Box';
 
-
 export const Statistics = ({ stats, title }) => {
-    return (<Box
-        width='300px'
-        mt='30px'
-        ml='auto'
-        mr='auto'
-        pt='10px'
-        bg='#f5f4fa'
-        textAlign='center'
-        borderRadius='8px'
-        boxShadow='0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
-    0px 2px 1px rgba(0, 0, 0, 0.2)'
-        as='section'
+  return (
+    <Box
+      width="300px"
+      mt="30px"
+      ml="auto"
+      mr="auto"
+      pt="10px"
+      bg="#f5f4fa"
+      textAlign="center"
+      borderRadius="8px"
+      boxShadow="0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
+    0px 2px 1px rgba(0, 0, 0, 0.2)"
+      as="section"
     >
-        <Title>{title}</Title>
+      {title && <Title>{title}</Title>}
 
-        <Box
-            display='flex'
-            borderTop='1px solid #5F7661'
-            as='ul'
-        >
-            {stats.map(({ id, label, percentage }) => (
-                <Item key={id}>
-                    <Label>{label} </Label>
-                    <Percentage>{percentage}%</Percentage>
-                </Item>))}
-        </Box>
+      <Box display="flex" borderTop="1px solid #5F7661" as="ul">
+        {stats.map(({ id, label, percentage }) => (
+          <Item key={id}>
+            <Label>{label} </Label>
+            <Percentage>{percentage}%</Percentage>
+          </Item>
+        ))}
+      </Box>
     </Box>
-    )
+  );
 };
 
-
 Statistics.propTypes = {
-    title: PropTypes.string.isRequired,
-    stats: PropTypes.arrayOf(
-        PropTypes.exact({
-            id: PropTypes.string.isRequired,
-            label: PropTypes.string.isRequired,
-            percentage: PropTypes.number.isRequired,
-        })
-    )
-}
+  title: PropTypes.string.isRequired,
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
